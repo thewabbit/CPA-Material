cc443101e31a86cd6462f4928a9f6ef575548601
Hamish <Hamish.Kingsbury@outlook.com>
2023-04-29 22:00:43 +1200

    added in template scan

--- createCert2.py
+++ createCert2.py
@@ -1,13 +1,13 @@
 import os
-import openpyxl
-from docx import Document
-from docx2pdf import convert
-from PyPDF2 import PdfMerger
+import random
 import tkinter as tk
-from tkinter import filedialog
-from tkinter import scrolledtext
-from tkinter import ttk
-from tkinter import *
+from tkinter import filedialog, scrolledtext, Label, Button
+from docx import Document
+from PyPDF2 import PdfMerger
+from docx2pdf import convert
+import openpyxl
+import re
+import uuid
 
 class CPA_Cert_Generator (tk.Tk):
     def __init__(self):
@@ -15,12 +15,25 @@ class CPA_Cert_Generator (tk.Tk):
 
         super().__init__()
 
+        self.lifterData = []
+        self.GUID = ""
+
         # Set the initial values of the template and lifterData attributes to None.
-        self.template = None
-        self.lifterData = None
+        self.templateNames = {
+            "certificateTemplate":"3LIFT CERT TEMPLATE.docx",
+            "speakerTemplate":"SPEAKER TEMPLATE.docx"
+        }
+
+        self.certificateTemplate = None
+        self.speakerTemplate = None
+        self.lifterDataInput = None
+
+        # testing
+        # self.certificateTemplate = r'G:\My Drive\Documents\Powerlifting\CPA\Create Certificates\Templates\3LIFT CERT TEMPLATE.docx'
+        # self.lifterDataInput = r'G:\My Drive\Documents\Powerlifting\CPA\Create Certificates\Inputs\Copy of Provincials 2023 final nominations2.xlsx'
 
         # Set the size, background color, and title of the window.
-        self.geometry('580x470')
+        self.geometry('580x580')
         self.configure(background='#FAEBD7')
         self.title('CPA Certificate Generator')
 
@@ -32,60 +45,77 @@ class CPA_Cert_Generator (tk.Tk):
 
         # Set up the logging text box and configure it with tags for different message types.
         self.logBox = scrolledtext.ScrolledText(self, wrap=tk.WORD, width=60, height=10, font=self.logFont)
-        self.logBox.place(x=40, y=278)
+        self.logBox.place(x=40, y=318)
         self.logBox.insert(tk.INSERT, 'Please select files \n')
         self.logBox.configure(state='disabled')
         self.logBox.tag_configure('error', foreground='red')
         self.logBox.tag_configure('warning', foreground='orange')
 
-        # Set up the labels for displaying the selected template and lifter data file paths.
-        self.selectedTemplate = Label(self, text='', bg='#FAEBD7', font=self.pathFont)
-        self.selectedTemplate.place(x=230, y=78)
-        self.selectedLifterData = Label(self, text='', bg='#FAEBD7', font=self.pathFont)
-        self.selectedLifterData.place(x=260, y=158)
 
-        # Set up the buttons for selecting the template and lifter data files.
-        self.templateButton = Button(self, text='Select Template File', bg='#FAEBD7', font=self.buttonFont, command=self.selectTemplate)
-        self.templateButton.place(x=40, y=38)
-        self.lifterDataButton = Button(self, text='Select Lifter Data', bg='#FAEBD7', font=self.buttonFont, command=self.selectLifterData)
-        self.lifterDataButton.place(x=40, y=118)
-        self.runButton = Button(self, text='Generate Certificate', bg='#FAEBD7', font=self.buttonFont, command=self.check)
-        self.runButton.place(x=40, y=198)
+        def create_button(parent, name, text, x, y, command):
+            button = Button(parent, name=name, text=text, bg='#FAEBD7', font=self.buttonFont, command=command)
+            button.place(x=x, y=y)
+            return button
+
+        def create_label(parent, name, text, x, y):
+            label = Label(parent, name=name, text=text, bg='#FAEBD7', font=self.pathFont)
+            label.place(x=x, y=y)
+            return label
+
+        self.LifterDataButton = create_button(self, 'lifterDataButton', 'Select Lifter Data', 40, 38, self.selectLifterData)
+        self.selectedLifterData = create_label(self, 'selectedLifterData', '', 40, 78)
+
+        self.certificateTemplateButton = create_button(self, 'certificateTemplateButton', 'Select Certificate Template File', 40, 118, self.selectCertificateTemplate)
+        self.selectedCertificateTemplate = create_label(self, 'selectedCertificateTemplate', '', 40, 158)
+
+        self.speakerTemplateButton = create_button(self, 'speakerTemplateButton', 'Select Speaker Card Template File', 40, 198, self.selectSpeakerTemplate)
+        self.selectedSpeakerTemplate = create_label(self, 'selectedSpeakerTemplate', '', 40, 238)
+
+
+        self.runButton = Button(self, text='Create files', bg='#FAEBD7', font=self.buttonFont, command=self.run)
+        self.runButton.place(x=40, y=278)
+
+        self.templateScan()
+
+    def templateScan(self):
+        for k,v in self.templateNames.items():
+            path = os.path.join(os.getcwd(),"Templates",v)
+            if os.path.exists(path):
+                setattr(self,k,path)
+                v1 = f"selected{k[0].upper() + k[1:]}"
+                getattr(self, v1).configure(text = v)
+
 
     def selectLifterData(self):
-        """
-        Opens a file dialog box to allow the user to select the lifter data Excel file.
-        Sets the selected file path to the class variable self.lifterData.
-        Updates the GUI to show the selected file name.
-        Logs the selected file path in the log box.
-        """
         # Open a file dialog box to allow the user to select the lifter data Excel file
-        self.lifterData = filedialog.askopenfilename(filetypes=[('Lifter data', '*.xlsx')])
+        self.lifterDataInput = filedialog.askopenfilename(filetypes=[('Lifter data', '*.xlsx')])
         
         # Update the GUI to show the selected file name
-        self.selectedLifterData.configure(text = os.path.split(self.lifterData)[1])
+        self.selectedLifterData.configure(text = os.path.split(self.lifterDataInput)[1])
         
         # Log the selected file path in the log box
-        self.log(f'selected input data:\n{self.lifterData}')
+        self.log(f'selected input data:\n{self.lifterDataInput}')
 
-
-    def selectTemplate(self):
-        """
-        Opens a file dialog box to allow the user to select the certificate template Word file.
-        Sets the selected file path to the class variable self.template.
-        Updates the GUI to show the selected file name.
-        Logs the selected file path in the log box.
-        """
+    def selectCertificateTemplate(self):
         # Open a file dialog box to allow the user to select the certificate template Word file
-        self.template = filedialog.askopenfilename(filetypes=[('Template file', '*.docx')])
+        self.certificateTemplate = filedialog.askopenfilename(filetypes=[('Template file', '*.docx')])
         
         # Update the GUI to show the selected file name
-        self.selectedTemplate.configure(text = os.path.split(self.template)[1])
+        self.selectedCertificateTemplate.configure(text = os.path.split(self.certificateTemplate)[1])
         
         # Log the selected file path in the log box
-        self.log(f'selected template:\n{self.template}')
+        self.log(f'selected template:\n{self.certificateTemplate}')
 
-  
+    def selectSpeakerTemplate(self):
+        # Open a file dialog box to allow the user to select the certificate template Word file
+        self.speakerTemplate = filedialog.askopenfilename(filetypes=[('Template file', '*.docx')])
+        
+        # Update the GUI to show the selected file name
+        self.selectedSpeakerTemplate.configure(text = os.path.split(self.speakerTemplate)[1])
+        
+        # Log the selected file path in the log box
+        self.log(f'selected template:\n{self.speakerTemplate}')
+
     def log(self, message, level='info'):
         """
         Logs a message to the GUI log box.
@@ -108,44 +138,87 @@ class CPA_Cert_Generator (tk.Tk):
         # Updates the GUI
         self.update()
 
+    def proccessData(self):
+        self.log('compiling lifter input data')
 
-    def processData(self):
-        self.log('running.....')
-
-        wb = openpyxl.load_workbook(self.lifterData)
+        # Load the Excel workbook and select the active worksheet
+        wb = openpyxl.load_workbook(self.lifterDataInput)
         ws = wb.active
 
-        doc = Document(self.template)
+        # Iterate through each row of data in the worksheet
+        for row in ws.iter_rows(min_row=2, values_only=True):
+            # Check if the first column of the row is not None
+            if row[0] != None:
+                lifterDataTemplate = {
+                    "Day": row[0],
+                    "Flight": row[1],
+                    "Age": re.sub(r'\([^)]*\)', '', row[2]),
+                    "Weight": re.sub(r'^.*\s-\s', '', row[3]),
+                    "First name": row[4],
+                    "Last name": row[5],
+                    "Gender": row[6],
+                    "Date Of Birth": row[7],
+                    "Association": row[8],
+                    "Raw": row[9],
+                    "Instagram": row[10],
+                    "Notes": row[11],
+                    "ID": random.randint(0, 100000)
+                }
 
+                self.lifterData.append(lifterDataTemplate)
+
+    def createCetificates(self):
+        """
+        Processes the data in the Excel workbook and generates certificates for each row of data.
+        Merges all generated certificates into a single PDF file.
+
+        Returns:
+        None
+        """
+
+        # Create a new Word document based on the provided template
+        doc = Document(self.certificateTemplate)
+
+        # Initialize a list to store the paths of all generated PDFs
         allPDFs = []
 
-        for row in ws.iter_rows(min_row=2, values_only=True):
-            if row[0] != None:
-                #data tuple =  name, weight, gender, age, equipment
-                #data.append((row[0],row[2],row[3],row[4],row[5]))
-                self.findReplaceTable(doc,dict(zzNAMEzz=row[0],zzCLASSzz=row[2], zzGENDERzz=row[3], zzDIVzz=row[4], zzEQUIPzz=row[5]))
+        # Iterate through each row of data in the worksheet
+        for l in self.lifterData:
 
-                self.log(f'Generating certificate for {row[0]}')
+            name = f'{l["First name"]} {l["Last name"]}'
 
-                doc.save(f'c:\\temp\\certificate_{row[0]}.docx')
-                convert(f'c:\\temp\\certificate_{row[0]}.docx')
-                allPDFs.append(f'c:\\temp\\certificate_{row[0]}.pdf')
+            self.findReplaceTable(doc,dict(zzNAMEzz=name,zzCLASSzz=l["Weight"], zzGENDERzz=l["Gender"], zzDIVzz=l["Age"], zzEQUIPzz=l["Raw"]))
 
-                self.log(f'Generated certificate for {row[0]}')
-                #revert back to save opening/close doc lots
-                self.findReplaceTable(doc,dict(zzNAMEzz=row[0]),True)
+            # Log that a certificate is being generated for the current row
+            self.log(f'Generating certificate for {name}')
 
+            # Save the current document as a Word file and convert it to PDF
+            doc.save(f'c:\\temp\\{self.GUID}\\certificate_{name}.docx')
+            convert(f'c:\\temp\\{self.GUID}\\certificate_{name}.docx')
+            # Add the path of the generated PDF to the list
+            allPDFs.append(f'c:\\temp\\{self.GUID}\\certificate_{name}.pdf')
+
+            # Log that a certificate has been generated for the current row
+            self.log(f'Generated certificate for {name}')
+
+            # Replace placeholder values in the Word document with empty strings to revert back to the original template
+            self.findReplaceTable(doc,dict(zzNAMEzz=name,zzCLASSzz=l["Weight"], zzGENDERzz=l["Gender"], zzDIVzz=l["Age"], zzEQUIPzz=l["Raw"]), True)
+
+        # Log that the certificates are being merged into a single PDF
         self.log(f'Merging Certificate PDFs')
 
+        # Create a new PDF merger object
         merger = PdfMerger()
+
+        # Iterate through each PDF file path in the list and add it to the merger
         for pdf in allPDFs:
             merger.append(pdf)
 
+        # Write the merged PDF to a file
         merger.write('certificates.pdf')
 
-        self.log(f'Certificates Merged into one PDF ')
-
-
+        # Log that the certificates have been merged into a single PDF
+        self.log(f'Certificates merged into one PDF')
 
     def findReplaceTable(self, doc, data, revert=False):
         """
@@ -182,22 +255,12 @@ class CPA_Cert_Generator (tk.Tk):
                                         for find, replace in data.items():
                                             run.text = run.text.replace(find, replace)
 
-
-
-    def check(self):
-        """Checks if both lifterData and template have been selected. If both have been selected, processData() is called.
-        If either one or none of them has been selected, a log message is printed with a warning or error level depending on
-        the number of files selected.
-        """
-        if self.lifterData and self.template:
-            self.processData()
-        elif self.lifterData:
-            self.log("Please select a template file", 'warning')
-        elif self.template:
-            self.log("Please select lifter data", 'warning')
-        else:
-            self.log("Please select files", 'error')
-
+    def run(self):
+        self.proccessData()
+        self.GUID = str(uuid.uuid4())
+        os.mkdir(f'c:\\temp\\{self.GUID}\\')
+        self.createCetificates()
+        #self.createWeighIn()
 
 
 if __name__ == '__main__':
